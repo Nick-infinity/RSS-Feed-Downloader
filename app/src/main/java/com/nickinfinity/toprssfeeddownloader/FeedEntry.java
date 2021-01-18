00f68ac80d578a63c3c5e0db7f5e6587ea0923da
Nickinfinity <nikhilg.me@gmail.com>
2021-01-18 16:02:03 +0530

    FeedEntery: getters&setters for data feilds

---
+++ app/src/main/java/com/nickinfinity/toprssfeeddownloader/FeedEntry.java
@@ -0,0 +1,57 @@
+package com.nickinfinity.toprssfeeddownloader;
+
+public class FeedEntry {
+    private String name;
+    private String artist;
+    private  String summary;
+    private String relDate;
+    private String imageURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getRelDate() {
+        return relDate;
+    }
+
+    public void setRelDate(String relDate) {
+        this.relDate = relDate;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\n' +
+                ", artist='" + artist + '\n' +
+                ", relDate='" + relDate + '\n' +
+                ", imageURL='" + imageURL + '\n' ;
+    }
+}
